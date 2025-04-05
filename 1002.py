25f68b77e4006ecdd590b8bd407d038ac146bb95
Hasan-TechNinja <hasantechninja@gmail.com>
2025-04-05 21:20:51 +0600

    solved

---
+++ 1002.py
@@ -0,0 +1,5 @@
+pai = 3.14159
+R = float(input())
+formula = pai * R**2
+
+print(f"A={formula:.4f}")
---
+++ 1005.py
@@ -0,0 +1,12 @@
+A = float(input())
+B = float(input())
+
+weightA = 3.5
+weightB = 7.5
+
+A = A * weightA
+B = B * weightB
+
+avg = (A + B) / (3.5 + 7.5)
+
+print(f"MEDIA = {avg:.5f}")
